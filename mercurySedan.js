61ebed01e6b8ef9fb2a25a7714832e8cce0b8e07
deankp <dperez0689@gmail.com>
2022-04-21 22:52:49 -0500

    added a few classes

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,6 +1,50 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
+class Car extends Vehicle {
+    constructor () {
+        super()
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num) {
+        if (this.passenger < this.maximumPassengers){
+            if ((num + this.passenger) <= this.maximumPassengers) {
+                this.passenger = num;
+                return this.passenger;
+        } 
+    
+        else {
+            console.log(this.model + " " + this.make + " cannot take all passengers.");
+        }
+     }
+      else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+    start() {
+        if (this.fuel > 0){
+            console.log("Engine On!");
+            return this.started = true
+        }
+        else {
+            console.log("Out of Fuel!");
+            return this.started = false;
+        }
+    }
+    scheduleService() {
+        if (this.mileage > 30000){
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+}
+
+
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+// let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+// console.log(v.make)
